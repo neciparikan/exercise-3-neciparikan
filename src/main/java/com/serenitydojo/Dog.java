535c89aa6dbe487c5bc4586933921344def6a64c
Necip.Arikan@vanderlande.com <Necip.Arikan@vanderlande.com>
2023-04-04 00:01:12 +0200

    test case is added

--- src/main/java/com/serenitydojo/Dog.java
+++ src/main/java/com/serenitydojo/Dog.java
@@ -4,4 +4,23 @@ public class Dog {
     public String name;
     public String favoriteToy;
     public int age;
+
+    public Dog(String name, String favoriteToy, int age) {
+        this.name = name;
+        this.favoriteToy = favoriteToy;
+        this.age = age;
+
+    }
+
+    public Object getName() {
+        return name;
+    }
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
